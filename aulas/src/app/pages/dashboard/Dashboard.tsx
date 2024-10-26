3e3c081cea32ff5d3332c6e8d8bde7c003d95729
Gustavo <gustavo303a@gmail.com>
2024-10-26 10:39:23 -0300

    new

--- aulas/src/app/pages/dashboard/Dashboard.tsx
+++ aulas/src/app/pages/dashboard/Dashboard.tsx
@@ -1,12 +1,10 @@
-const Button = () => {
-  return <button>Meu button</button>;
-};
+import { Link } from "react-router-dom";
 
 export const Dashboard = () => {
   return (
     <div>
-      <p>Dashboard</p>;
-      <Button />
+      <p>Dashboard</p>
+      <Link to={"/entrar"}>Login</Link>
     </div>
   );
 };
--- aulas/src/app/routes/index.tsx
+++ aulas/src/app/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/entrar" Component={Login} />
         <Route path="/pagina-inicial" Component={Dashboard} />
 
         <Route path="*" Component={() => <Navigate to="/pagina-inicial" />} />
